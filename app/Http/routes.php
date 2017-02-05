619efc3d7fc27a7659a504fcac53c21624b97d71
h我叫小刀 <214564382@qq.com>
2017-02-05 17:59:12 +0800

    清理路由

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -35,18 +35,3 @@ Route::group(['middleware' => ['web', 'weicms.login'], 'prefix' => 'weicms', 'na
 //Route::get('/', function () {
 //    return view('welcome');
 //});
-
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
-
-Route::group(['middleware' => ['web']], function () {
-    //
-});
