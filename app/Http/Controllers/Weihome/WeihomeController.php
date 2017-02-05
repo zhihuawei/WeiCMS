97644d187557dec17c8e80908e2fa1081abbc51a
h我叫小刀 <214564382@qq.com>
2017-02-05 16:31:38 +0800

    前台父类控制器

---
+++ app/Http/Controllers/Weihome/WeihomeController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * =================================================
+ * Created by Pocket Knife Technology.
+ * Author: Wei ZhiHua <zhihua_wei@foxmail.com>
+ * Date: 2017/2/5 0005
+ * Time: 下午 4:23
+ * Project: WeiCMS
+ * Power: 前台父类控制器
+ * =================================================
+ */
+
+namespace App\Http\Controllers\Weihome;
+
+use App\Http\Controllers\Controller;
+
+class WeihomeController extends Controller{
+    public function __construct()
+    {
+        
+    }
+}
+ 
