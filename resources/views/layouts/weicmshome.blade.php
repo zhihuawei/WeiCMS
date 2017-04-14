fec2c2c3f5cce161755200a31f6d7af3be422754
h我叫小刀 <214564382@qq.com>
2017-04-14 15:44:52 +0800

    20170414新建前台blade框架

--- resources/views/layouts/weicmshome.blade.php
+++ resources/views/layouts/weicmshome.blade.php
@@ -1,12 +1,250 @@
-{{--/**--}}
- {{--* =======================================--}}
- {{--* Created by WeiBang Technology.--}}
- {{--* Author: Wei ZhiHua--}}
- {{--* Date: 2017/4/14 0014--}}
- {{--* Time: 下午 3:30--}}
- {{--* Project: WeiCMS--}}
- {{--* Version: 1.0.0--}}
- {{--* Power: 前台blade框架--}}
- {{--* =================================================--}}
- {{--*/--}}
- 
+<!DOCTYPE html>
+<html lang="zh-CN">
+
+<head>
+    <meta name="Generator" content="CmsEasy 5_6_0_20170105_UTF8"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
+    <meta name="renderer" content="webkit">
+    <!-- 解决国内双核浏览器优先试用webkit的问题 -->
+    <title>WeiCMS 快速建站系统</title>
+    <!-- Bootstrap -->
+    <link href="demo/business-template/V387/template/CmsEasy_V_387/skin/css/bootstrap.min.css" rel="stylesheet">
+    <link href="demo/business-template/V387/template/CmsEasy_V_387/skin/css/style.css" rel="stylesheet">
+    <link href="demo/business-template/V387/template/CmsEasy_V_387/skin/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="demo/business-template/V387/template/CmsEasy_V_387/skin/css/animate.min.css">
+
+    <script src="demo/business-template/V387/template/CmsEasy_V_387/skin/js/jquery-1.11.3.min.js"
+            type="text/javascript"></script>
+    <script src="demo/business-template/V387/template/CmsEasy_V_387/skin/js/jquery.SuperSlide.2.1.1.js"
+            type="text/javascript"></script>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <script src="demo/business-template/V387/template/CmsEasy_V_387/skin/js/wow.min.js"></script>
+    <script>
+        if (!(/msie [6|7|8|9]/i.test(navigator.userAgent))) {
+            new WOW().init();
+        }
+    </script>
+
+
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="/cdn.bootcss.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="/cdn.bootcss.com/respond./demo/business-template/V387/template/CmsEasy_V_387/skin/js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<div class="hidden-xs">
+    <div id="header" class="clearfix">
+        <div class="logo-box">
+            <a href="demo/business-template/V387/"><img src="images/logo.png"/></a>
+        </div>
+        <div class="menu">
+            <ul>
+                <li>
+                    <a href="index.html" class="on">网站首页</a>
+                </li>
+                <li>
+                    <a href="fuzhuang.html" class="">信息</a>
+                    <div class="menu2">
+                        <ul>
+                            <li>
+                                <a href="demo/business-template/V387/chang-xiu">信息</a>
+                            </li>
+                            <li>
+                                <a href="demo/business-template/V387/tixu">信息</a>
+                            </li>
+                            <li>
+                                <a href="demo/business-template/V387/qiu-dong">信息</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <a href="aboutus.html" class="">关于</a>
+                    <div class="menu2">
+                        <ul>
+                            <li>
+                                <a href="demo/business-template/V387/about_us">关于我们</a>
+                            </li>
+                            <li>
+                                <a href="news.html">新闻</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+                <li>
+                    <a href="demo/business-template/V387/pin-pai-zhou-bian" class="">周边</a>
+                </li>
+                <li>
+                    <a href="pingpaigushi.html" class="">故事</a>
+                </li>
+                <li>
+                    <a href="lianxi.html" class="">联系</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <script src="demo/business-template/V387/template/CmsEasy_V_387/skin/js/headermini.js"></script>
+</div>
+
+
+<div class="hidden-sm hidden-md hidden-lg">
+    <div class="hidden-xs">
+        <div id="header" class="clearfix">
+            <div class="logo-box">
+                <a href="demo/business-template/V387/"><img src="images/logo.png"/></a>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li>
+                        <a href="index.html" class="on">网站首页</a>
+                    </li>
+                    <li>
+                        <a href="fuzhuang.html" class="">信息</a>
+                        <div class="menu2">
+                            <ul>
+                                <li>
+                                    <a href="demo/business-template/V387/chang-xiu">信息</a>
+                                </li>
+                                <li>
+                                    <a href="demo/business-template/V387/tixu">信息</a>
+                                </li>
+                                <li>
+                                    <a href="demo/business-template/V387/qiu-dong">信息</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="aboutus.html" class="">关于</a>
+                        <div class="menu2">
+                            <ul>
+                                <li>
+                                    <a href="demo/business-template/V387/about_us">关于我们</a>
+                                </li>
+                                <li>
+                                    <a href="news.html">新闻</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <a href="demo/business-template/V387/pin-pai-zhou-bian" class="">周边</a>
+                    </li>
+                    <li>
+                        <a href="pingpaigushi.html" class="">故事</a>
+                    </li>
+                    <li>
+                        <a href="lianxi.html" class="">联系</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <script src="demo/business-template/V387/template/CmsEasy_V_387/skin/js/headermini.js"></script>
+    </div>
+
+
+    <div class="hidden-sm hidden-md hidden-lg">
+        <div class="hidden-xs">
+            <div id="header" class="clearfix">
+                <div class="logo-box">
+                    <a href="demo/business-template/V387/"><img src="images/logo.png"/></a>
+                </div>
+                <div class="menu">
+                    <ul>
+                        <li>
+                            <a href="index.html" class="on">网站首页</a>
+                        </li>
+                        <li>
+                            <a href="fuzhuang.html" class="">信息</a>
+                            <div class="menu2">
+                                <ul>
+                                    <li>
+                                        <a href="demo/business-template/V387/chang-xiu">信息</a>
+                                    </li>
+                                    <li>
+                                        <a href="demo/business-template/V387/tixu">信息</a>
+                                    </li>
+                                    <li>
+                                        <a href="demo/business-template/V387/qiu-dong">信息</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="aboutus.html" class="">关于</a>
+                            <div class="menu2">
+                                <ul>
+                                    <li>
+                                        <a href="demo/business-template/V387/about_us">关于我们</a>
+                                    </li>
+                                    <li>
+                                        <a href="news.html">新闻</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="demo/business-template/V387/pin-pai-zhou-bian" class="">周边</a>
+                        </li>
+                        <li>
+                            <a href="pingpaigushi.html" class="">故事</a>
+                        </li>
+                        <li>
+                            <a href="lianxi.html" class="">联系</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <script src="demo/business-template/V387/template/CmsEasy_V_387/skin/js/headermini.js"></script>
+        </div>
+
+
+        <div class="hidden-sm hidden-md hidden-lg">
+
+
+            <!--<script type="text/javascript" src="demo/business-template/V387/template/CmsEasy_V_387/skin/js/common.js"></script>-->
+            <script>
+                window._bd_share_config = {
+                    "common": {
+                        "bdSnsKey": {},
+                        "bdText": "",
+                        "bdMini": "2",
+                        "bdMiniList": false,
+                        "bdPic": "",
+                        "bdStyle": "0",
+                        "bdSize": "16"
+                    },
+                    "slide": {
+                        "type": "slide",
+                        "bdImg": "6",
+                        "bdPos": "right",
+                        "bdTop": "100"
+                    },
+                };
+                with (document) 0[(getElementsByTagName('head')[0] || body).appendChild(createElement('script')).src = 'http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion=' + ~(-new Date() / 36e5)];
+            </script>
+
+            <!-- Bootstrap core JavaScript
+                ================================================== -->
+            <!-- Placed at the end of the document so the pages load faster -->
+
+            <script src="demo/business-template/V387/template/CmsEasy_V_387/skin/js/bootstrap.min.js"></script>
+            <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+            <script src="demo/business-template/V387/template/CmsEasy_V_387/skin/js/holder.min.js"></script>
+            <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+            <script src="demo/business-template/V387/template/CmsEasy_V_387/skin/js/ie10-viewport-bug-workaround.js"></script>
+            <script src="demo/business-template/V387/template/CmsEasy_V_387/skin/js/bootstrap-submenu.js"></script>
+            <link rel="stylesheet" href="demo/business-template/V387/template/CmsEasy_V_387/skin/css/animate.min.css"/>
+            <!--<script src="demo/business-template/V387/template/CmsEasy_V_387/skin/js/wow.min.js"></script>
+            <script>
+                if(!(/msie [6|7|8|9]/i.test(navigator.userAgent))) {
+                    new WOW().init();
+                };
+            </script>-->
+</body>
+
+</html>
