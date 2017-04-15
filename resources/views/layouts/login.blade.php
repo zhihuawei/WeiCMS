6b9d1c5c0ccacf15796c7cf365e01e0e0b996788
h我叫小刀 <214564382@qq.com>
2017-04-15 15:40:10 +0800

    20170415登录框架模版

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -1,2 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>WeiCMS 登录</title>
+    <link rel="stylesheet" href="{{asset('resources/assets/weicms/css/style.default.css')}}" type="text/css" />
+    <script type="text/javascript" src="{{asset('resources/assets/weicms/js/plugins/jquery-1.7.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('resources/assets/weicms/js/plugins/jquery-ui-1.8.16.custom.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('resources/assets/weicms/js/plugins/jquery.cookie.js')}}"></script>
+    <script type="text/javascript" src="{{asset('resources/assets/weicms/js/plugins/jquery.uniform.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('resources/assets/weicms/js/custom/general.js')}}"></script>
+    <script type="text/javascript" src="{{asset('resources/assets/weicms/js/custom/login.js')}}"></script>
+    <!--[if IE 9]>
+        <link rel="stylesheet" media="screen" href="{{asset('resources/assets/weicms/css/style.ie9.css')}}"/>
+    <![endif]-->
+    <!--[if IE 8]>
+        <link rel="stylesheet" media="screen" href="{{asset('resources/assets/weicms/css/style.ie8.css')}}"/>
+    <![endif]-->
+    <!--[if lt IE 9]>
+        <script src="{{asset('resources/assets/weicms/js/plugins/css3-mediaqueries.js')}}"></script>
+    <![endif]-->
+</head>
 
+<body class="loginpage">
+
+@yield('main-content')
+
+
+</body>
+</html>
  
